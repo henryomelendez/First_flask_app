f8531e2734054136f2f4a1798688b503d18ab723
henryomelendez <147175644+henryomelendez@users.noreply.github.com>
2023-12-06 20:38:50 -0500

    Update index.js
    made changes to interact with database

--- src/my_project/static/index.js
+++ src/my_project/static/index.js
@@ -1,4 +1,4 @@
-function addTask() {
+/* function addTask() {
     const task = document.getElementsByClassName("section1")[0].querySelector("input").value;
     
     let  li = document.createElement("li");
@@ -30,8 +30,51 @@ function addTask() {
     doclist.appendChild(li);
     
 }
+*/
+
+window.onload = function() {
+    fetch('/tasks')
+    .then(response => response.json())
+    .then(tasks => {
+        for (let i = 0; i < tasks.length; i++) {
+            var li = document.createElement('li');
+            li.setAttribute('data-id', tasks[i].id);
+            var label = document.createElement('label');
+            label.textContent = tasks[i].task;
+            li.appendChild(label);
+            if (tasks[i].status === '1') {
+                document.getElementById('completed-tasks').appendChild(li);
+            } else {
+                document.getElementById('incomplete-tasks').appendChild(li);
+            }
+        }
+    });
+}
 
 
+
+document.getElementById('submit-task').addEventListener('click', function(e) {
+    e.preventDefault();
+    var taskInput = document.getElementById('task');
+    var task = taskInput.value;
+    taskInput.value = '';
+    fetch('/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'task=' + encodeURIComponent(task),
+    })
+    .then(response => response.json())
+    .then(data => {
+        var li = document.createElement('li');
+        var label = document.createElement('label');
+        label.textContent = data.task;
+        li.appendChild(label);
+        document.getElementById('incomplete-tasks').appendChild(li);
+    });
+});
+/*
 function editTask(){
     let li = this.parentNode;
     let label = li.querySelector("label");
@@ -55,12 +98,69 @@ function editTask(){
     }
     li.classList.toggle("editMode");
 }
+*/
 
+function editTask() {
+    var li = this.parentNode;
+    var label = li.querySelector("label");
+    var input = li.querySelector("input[type=text]");
+    var id = li.getAttribute('data-id'); 
+
+    var containclass = li.classList.contains("editMode");
+    if (containclass) {
+        
+        label.innerText = input.value;
+        fetch('/update/' + id, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'task=' + encodeURIComponent(input.value),
+        });
+    } else {
+        // switch to .editMode
+        // input value becomes the label's text
+        input.value = label.innerText;
+    }
+
+    // Toggle .editMode on the parent
+    li.classList.toggle("editMode");
+}
+
+/*
 function deleteTask(){
     let li = this.parentNode;
     let ul = li.parentNode;
     ul.removeChild(li);
 }
+*/
+
+document.querySelectorAll('.delete-button').forEach(button => {
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        var li = this.parentNode;
+        var id = li.getAttribute('data-id'); 
+
+        fetch('/delete/' + id, {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                alert(data.error);
+            } else {
+                
+                li.parentNode.removeChild(li);
+            }
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+    });
+});
+
+
+
 
 function bindTaskEvents(taskListItem){
     let editButton = taskListItem.querySelector("button.edit");
